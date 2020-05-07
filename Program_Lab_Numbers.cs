639abc85549f33585fb6c97c0649271034962da4
Caner Demirci <caner.demirci@outlook.com>
2020-05-07 19:12:21 +0300

    Create Program_Lab_Numbers.cs

---
+++ Program_Lab_Numbers.cs
@@ -0,0 +1,48 @@
+/*
+** Verilen sayıdan küçük sayılar arasında
+** asal bölen bir sayının karesi verilen sayıyı
+** tam bölüyorsa o sayı lab number dır.
+*/
+using System;
+using System.Collections;
+
+class Program_Lab_Numbers
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[5];
+
+            Console.WriteLine("Input 5 number: ");
+
+            for (var i=0; i<5; i++)
+                numbers[i] = int.Parse(Console.ReadLine());
+            
+            for (var i=0; i<5; i++)
+                Console.WriteLine(
+                    $"{numbers[i]} is " + 
+                    (string)(isLabNumber(numbers[i]) == true ? "Lab number" : "not lab number")
+                );
+        }
+        
+        static bool isLabNumber(int n)
+        {
+            for (var i=2; i<n; i++)
+                if (isPrime(i) && n % i == 0)
+                    if (n % (i * i) == 0)
+                        return true;
+            
+            return false;
+        }
+
+        static bool isPrime(int n)
+        {
+            if (n < 2)
+                return false;
+            
+            for (var i=n-1; i>=2; i--)
+                if (n % i == 0)
+                    return false;
+            
+            return true;
+        }
+    }
